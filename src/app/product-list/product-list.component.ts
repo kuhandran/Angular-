d76b1e69a5a1a6daa210a635bb4c13707d7c7a6d
kuhan <skuhandran@yahoo.com>
2019-07-21 23:27:07 +0800

    alert messages with bugs cannot open the components

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,11 +6,16 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list-component.scss']
 })
+
 export class ProductListComponent {
   products = products;
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
